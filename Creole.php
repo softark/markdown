a4758717768233d35b6ab34d9fd834f1697408d4
Nobuo Kihara <softark@gmail.com>
2015-03-05 07:53:42 +0900

    Creole を作成開始

---
+++ Creole.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * @copyright Copyright (c) 2014 Carsten Brandt / 2015 Nobuo Kihara
+ * @license https://github.com/cebe/markdown/blob/master/LICENSE
+ * @link https://github.com/cebe/markdown#readme
+ */
+
+namespace cebe\markdown;
+
+/**
+ * Creole wiki parser for [Creole 1.0 spec](http://www.wikicreole.org/wiki/Creole1.0).
+ *
+ * @author Nobuo Kihara <softark@gmail.com>
+ */
+class Creole extends Parser
+{
+//	// include block element parsing using traits
+//	use block\CodeTrait;
+//	use block\HeadlineTrait;
+//	use block\HtmlTrait {
+//		parseInlineHtml as private;
+//	}
+//	use block\ListTrait {
+//		// Check Ul List before headline
+//		identifyUl as protected identifyBUl;
+//		consumeUl as protected consumeBUl;
+//	}
+//	use block\QuoteTrait;
+//	use block\RuleTrait {
+//		// Check Hr before checking lists
+//		identifyHr as protected identifyAHr;
+//		consumeHr as protected consumeAHr;
+//	}
+//
+//	// include inline element parsing using traits
+//	use inline\CodeTrait;
+//	use inline\EmphStrongTrait;
+//	use inline\LinkTrait;
+
+	/**
+	 * @var boolean whether to format markup according to HTML5 spec.
+	 * Defaults to `false` which means that markup is formatted as HTML4.
+	 */
+	public $html5 = false;
+
+	/**
+	 * @var array these are "escapeable" characters. When using one of these prefixed with a
+	 * backslash, the character will be outputted without the backslash and is not interpreted
+	 * as markdown.
+	 */
+	protected $escapeCharacters = [
+		'\\', // backslash
+		'`', // backtick
+		'*', // asterisk
+		'_', // underscore
+		'{', '}', // curly braces
+		'[', ']', // square brackets
+		'(', ')', // parentheses
+		'#', // hash mark
+		'+', // plus sign
+		'-', // minus sign (hyphen)
+		'.', // dot
+		'!', // exclamation mark
+		'<', '>',
+	];
+
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function prepare()
+	{
+		// reset references
+		$this->references = [];
+	}
+
+	/**
+	 * Consume lines for a paragraph
+	 *
+	 * Allow headlines and code to break paragraphs
+	 */
+	protected function consumeParagraph($lines, $current)
+	{
+		// consume until newline
+		$content = [];
+		for ($i = $current, $count = count($lines); $i < $count; $i++) {
+			$line = $lines[$i];
+			if (!empty($line) && ltrim($line) !== '' &&
+				!($line[0] === "\t" || $line[0] === " " && strncmp($line, '    ', 4) === 0) /*&&
+				!$this->identifyHeadline($line, $lines, $i)*/)
+			{
+				$content[] = $line;
+			} else {
+				break;
+			}
+		}
+		$block = [
+			'paragraph',
+			'content' => $this->parseInline(implode("\n", $content)),
+		];
+		return [$block, --$i];
+	}
+
+
+	/**
+	 * Parses escaped special characters.
+	 * Creole uses tilde (~) for the escaping marker.
+	 * @marker ~
+	 */
+	protected function parseEscape($text)
+	{
+		if (isset($text[1]) && in_array($text[1], $this->escapeCharacters)) {
+			return [['text', $text[1]], 2];
+		}
+		return [['text', $text[0]], 1];
+	}
+
+    /**
+	 * @inheritdocs
+	 *
+	 * Parses a newline indicated by two backslashes on the end of a creole line.
+	 */
+	protected function renderText($text)
+	{
+		return str_replace("\\\\\n", $this->html5 ? "<br>\n" : "<br />\n", $text[1]);
+	}
+}
---
+++ tests/CreoleTest.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * @copyright Copyright (c) 2014 Carsten Brandt / 2015 Nobuo Kihara
+ * @license https://github.com/cebe/markdown/blob/master/LICENSE
+ * @link https://github.com/cebe/markdown#readme
+ */
+
+namespace cebe\markdown\tests;
+
+use cebe\markdown\Parser;
+use cebe\markdown\Creole;
+
+/**
+ * Base class for all Test cases.
+ *
+ * @author Carsten Brandt <mail@cebe.cc>
+ */
+class CreoleTest extends \PHPUnit_Framework_TestCase
+{
+	protected $outputFileExtension = '.html';
+
+	public function getDataPaths()
+	{
+		return [
+			'creole-data' => __DIR__ . '/creole-data',
+		];
+	}
+
+	/**
+	 * @return Parser
+	 */
+	public function createCreole()
+	{
+		return new Creole();
+	}
+
+	/**
+	 * @dataProvider dataFiles
+	 */
+	public function testParse($path, $file)
+	{
+		list($markdown, $html) = $this->getTestData($path, $file);
+		// Different OS line endings should not affect test
+		$html = str_replace(["\r\n", "\n\r", "\r"], "\n", $html);
+
+		$m = $this->createCreole();
+		$this->assertEquals($html, $m->parse($markdown));
+	}
+
+	public function testUtf8()
+	{
+		$this->assertSame("<p>абвгдеёжзийклмнопрстуфхцчшщъыьэюя</p>\n", $this->createCreole()->parse('абвгдеёжзийклмнопрстуфхцчшщъыьэюя'));
+		$this->assertSame("<p>there is a charater, 配</p>\n", $this->createCreole()->parse('there is a charater, 配'));
+		$this->assertSame("<p>Arabic Latter \"م (M)\"</p>\n", $this->createCreole()->parse('Arabic Latter "م (M)"'));
+		$this->assertSame("<p>電腦</p>\n", $this->createCreole()->parse('電腦'));
+
+		$this->assertSame('абвгдеёжзийклмнопрстуфхцчшщъыьэюя', $this->createCreole()->parseParagraph('абвгдеёжзийклмнопрстуфхцчшщъыьэюя'));
+		$this->assertSame('there is a charater, 配', $this->createCreole()->parseParagraph('there is a charater, 配'));
+		$this->assertSame('Arabic Latter "م (M)"', $this->createCreole()->parseParagraph('Arabic Latter "م (M)"'));
+		$this->assertSame('電腦', $this->createCreole()->parseParagraph('電腦'));
+	}
+
+//	public function testInvalidUtf8()
+//	{
+//		$m = $this->createCreole();
+//		$this->assertEquals("<p><code>�</code></p>\n", $m->parse("`\x80`"));
+//		$this->assertEquals('<code>�</code>', $m->parseParagraph("`\x80`"));
+//	}
+
+	public function pregData()
+	{
+		// http://en.wikipedia.org/wiki/Newline#Representations
+		return [
+			["a\r\nb", "a\nb"],
+			["a\n\rb", "a\nb"], // Acorn BBC and RISC OS spooled text output :)
+			["a\nb", "a\nb"],
+			["a\rb", "a\nb"],
+
+			["a\n\nb", "a\n\nb", "a</p>\n<p>b"],
+			["a\r\rb", "a\n\nb", "a</p>\n<p>b"],
+			["a\n\r\n\rb", "a\n\nb", "a</p>\n<p>b"], // Acorn BBC and RISC OS spooled text output :)
+			["a\r\n\r\nb", "a\n\nb", "a</p>\n<p>b"],
+		];
+	}
+
+	/**
+	 * @dataProvider pregData
+	 */
+	public function testPregReplaceR($input, $exptected, $pexpect = null)
+	{
+		$this->assertSame($exptected, $this->createCreole()->parseParagraph($input));
+		$this->assertSame($pexpect === null ? "<p>$exptected</p>\n" : "<p>$pexpect</p>\n", $this->createCreole()->parse($input));
+	}
+
+	public function getTestData($path, $file)
+	{
+		return [
+			file_get_contents($this->getDataPaths()[$path] . '/' . $file . '.md'),
+			file_get_contents($this->getDataPaths()[$path] . '/' . $file . $this->outputFileExtension),
+		];
+	}
+
+	public function dataFiles()
+	{
+		$files = [];
+		foreach ($this->getDataPaths() as $name => $src) {
+			$handle = opendir($src);
+			if ($handle === false) {
+				throw new \Exception('Unable to open directory: ' . $src);
+			}
+			while (($file = readdir($handle)) !== false) {
+				if ($file === '.' || $file === '..') {
+					continue;
+				}
+
+				if (substr($file, -3, 3) === '.md' && file_exists($src . '/' . substr($file, 0, -3) .  $this->outputFileExtension)) {
+					$files[] = [$name, substr($file, 0, -3)];
+				}
+			}
+			closedir($handle);
+		}
+		return $files;
+	}
+}
